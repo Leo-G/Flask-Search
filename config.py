8d8b742b4c7a18bfbfa461b10d081fe194a9e76b
Leo G <leo@leog.in>
2015-05-26 19:26:08 +0530

    config

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #DATABASE SETTINGS
 db_username = 'postgres'
-db_password = 'qwedsa@123'
+db_password = ''
 db_name = 'search1'
 db_hostname = 'localhost'
 
 DEBUG = True
 PORT = 5000
-HOST = "172.16.189.28"
+HOST = "localhost"
 SQLALCHEMY_ECHO = True
 SECRET_KEY = "c1987yhue25-88ec-4ec2-b2fe-4c213413jki89"
 SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER=db_username,
